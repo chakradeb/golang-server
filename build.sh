113839d741d5b789c57f1f71244be629fa4da6e7
Debarun Chakraborty <debarun.chakraborty96@gmail.com>
2018-06-13 23:16:02 +0530

    Now Tests will on build script

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cd app
+
+go test main_test.go main.go
+
 GOOS=linux GOARCH=amd64 go build -o ../build/server-linux
 GOOS=darwin GOARCH=amd64 go build -o ../build/server-darwin
+cd ..
 sh ./docker-publish.sh
--- utils/utils.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "net/http"
-
-type Handler interface {
-	HandleFunction(w http.ResponseWriter,r http.Request)
-}
